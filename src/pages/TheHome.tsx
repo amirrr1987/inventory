341e589e01e9409655cf7cd3f655ceabb4f9fa17
Amir Maghami <amaghami@haco.dc>
2022-10-03 17:20:59 +0330

    feat: main content

--- src/pages/TheHome.tsx
+++ src/pages/TheHome.tsx
@@ -1,30 +1,108 @@
 import Container from "../components/Container";
 const TheHome = () => {
   return (
+    <main>
+      <section className="py-12">
+        <Container>
+          <div className="grid grid-cols-3 gap-4">
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <section className="py-12 bg-blue-50">
+        <Container>
+          <div className="grid grid-cols-3 gap-4">
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+          </div>
+        </Container>
+      </section>
 
-      <main>
-        <section className="py-12">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12 bg-blue-50">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12 bg-blue-50">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12 bg-blue-50">
-          <Container>sdf</Container>
-        </section>
-        <section className="py-12">
-          <Container>sdf</Container>
-        </section>
-      </main>
+      <section className="py-12">
+        <Container>
+          <div className="grid grid-cols-3 gap-4">
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl capitalize border-b pb-2 mb-2">title</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                praesentium labore esse autem architecto officiis nihil atque
+                ipsam, mollitia facere voluptatem perspiciatis accusantium! Odio
+                totam hic nihil nulla atque. Reiciendis.
+              </p>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </main>
   );
 };
 export default TheHome;
